dcdc9240d2cc1fae6ddb44b4c01992705b974ac1
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-06-16 21:11:29 +0100

    features(codebase): included test task and coverage

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,16 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import jasmine from 'gulp-jasmine';
+import path from 'path';
+import nodemon from 'gulp-nodemon';
+import istanbul from 'gulp-istanbul';
+import coveralls from 'gulp-coveralls';
+import exit from 'gulp-exit';
 
-gulp.task('transpile', () =>
-  gulp.src(['./**/*.js', '!dist/**', '!node_modules/**', '!gulpfile.babel.js'])
+
+// this transpiles all .js files except those in dist and node_modules folders
+gulp.task('babel', () =>
+  gulp.src(['./**/*.js', '!dist/**', '!node_modules/**', '!gulpfile.babel.js', '!coverage/**'])
   .pipe(babel())
   .pipe(gulp.dest('dist'))
 );
@@ -12,4 +20,32 @@ gulp.task('run-tests', ['babel'], () => {
   gulp.src(path.join('dist', 'tests', 'inverted-index-test.js'))
   .pipe(jasmine())
   .pipe(exit());
+});
+
+// this starts the server at the specified port in .env file
+gulp.task('serve', ['babel'], () => {
+  nodemon({
+    script: path.join('dist', 'server.js'),
+    ext: 'js',
+    env: { NODE_ENV: 'development' }
+  });
+});
+
+// the coverage task depends on 'pre-test' and 'test' to give coverage report
+gulp.task('pre-test', () =>
+  gulp.src([path.join('dist', 'src', 'inverted-index.js')])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+);
+
+gulp.task('test', ['pre-test'], () =>
+  gulp.src([path.join('dist', 'tests', 'inverted-index-test.js')])
+    .pipe(jasmine())
+    .pipe(istanbul.writeReports())
+);
+
+gulp.task('coverage', ['test'], () => {
+  gulp.src('coverage/**/lcov.info')
+    .pipe(coveralls())
+    .pipe(exit());
 });
