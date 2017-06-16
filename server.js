c5908dba0edee597734bf93fc9875521aaeceb58
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-06-16 09:50:47 +0100

    features: included dotenv for environment variable

---
+++ server.js
@@ -0,0 +1,11 @@
+// This will be our application entry. We'll setup our server here.
+import app from './server/config/app'
+
+const port = process.env.PORT || 5000;
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`App running at http://localhost:${port}`);
+  }
+});
