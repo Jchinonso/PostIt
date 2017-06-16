0af621f7e8eb03db98c1cae4094b4ec959c2c49b
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-06-16 10:05:24 +0100

    feature: server config file

---
+++ server/config/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import logger from 'morgan'
+import bodyParser from 'body-parser'
+import Routes from '../routes/index'
+
+const app = express();
+const router = express.Router()
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+Routes(router);
+
+// prefix /api for all routes
+app.use('/api', router);
+
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+module.exports = app;
