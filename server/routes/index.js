accb9cdb085310b8ed64af5686295f90be2ed600
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-06-19 16:32:15 +0100

    combine all routes together

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 import UsersRoutes from './UserRoutes';
 import GroupRoutes from './GroupRoutes'
+import MessageRoutes from './MessageRoutes'
 
 
 // setup routes using router
 const Routes = (router) => {
   UsersRoutes(router);
-  GroupRoutes(router);
+  GroupRoutes(router)
+  MessageRoutes(router)
 };
 
 export default Routes;
