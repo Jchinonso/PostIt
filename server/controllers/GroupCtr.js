31ea170cd75d71de4abac0e6af7363ca93a738b6
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-06-19 16:19:34 +0100

    refactor: remove some implementation into messageCtr.js

--- server/controllers/GroupCtr.js
+++ server/controllers/GroupCtr.js
@@ -2,7 +2,6 @@
 import db from '../models';
 
 
-
 const GroupCtrl = {
 
   /**
@@ -11,19 +10,23 @@ const GroupCtrl = {
    * @param {Object} res Response Object
    * @returns {void} Returns void
    */
-  create(req, res) {
-     db.Groups.findOrCreate(
-      {where: {name: req.body.name}, defaults:
-       {description: req.body.description}})
-  .spread((group, created) => {
-    if(!created){
-      return res.status(409).send({message: "Group already exist"})
-    } else {
-      return res.status(200).send(group)
-    }
-  })
-      
+  createGroup(req, res) {
+     db.Groups.findOrCreate({
+       where: {name: req.body.name}, 
+       defaults: {
+         description: req.body.description
+        }})
+        .spread((group, created) => {
+          if(!created){
+             return res.status(409).send({
+               message: "Group already exist"
+              })}
+               else {
+                 return res.status(200).send(group)
+              }
+            })
    },
+
     
     /** Add User To Group
    * @param {Object} req Request Object
@@ -31,15 +34,14 @@ const GroupCtrl = {
    * @returns {object} Returns User
    */
 
-   addUser(req, res) {
+   addUserToGroup(req, res) {
      const id = req.params.id;
      db.Users.findOne({where:{username: req.body.username}}).then(user =>{
        if(user){
          const newUserId = user.id;
          db.UserGroups.findOrCreate(
-            {where: {userId: newUserId, groupId: id}, defaults:
-            {userId: newUserId,
-            groupId: id}})
+            {where: {userId: newUserId, groupId: id}, 
+            defaults:{userId: newUserId,groupId: id}})
         .spread((userGroup, created) => {
           if(!created){
             return res.status(409).send({message: "user already exist"})
@@ -47,11 +49,34 @@ const GroupCtrl = {
             return res.status(201).send(userGroup)
           }
         })
+       } else {
+         return res.status(404).send({message: "This User does not exist"})
        }
-     }).catch(e => {
-       return res.status(404).send({message: "This User does not exist"});
+     }).catch(err =>{
+       return res.status(500).send(err)
      })
-    }
+    },
+
+
+    /** Retrieves all Users of a Group
+   * @param {Object} req Request Object
+   * @param {Object} res Response Object
+   * @returns {object} Returns Group Users
+   */
+
+    retrieveGroupUsers(req, res){
+      db.UserGroups.findAll({
+        where: {
+          groupId: req.params.id
+        }}).then(users => {
+          return res.status(200).send(users);
+        })
+        .catch(err => {
+          return res.status(404).send(err);
+        })
+    },
+
+   
 }
  
 
