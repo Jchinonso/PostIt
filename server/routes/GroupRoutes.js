67d66c0042c238cee6efb664de6fa2fb9d8c2aed
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-06-28 13:33:28 +0100

    refactor: Post group/:id/user

--- server/routes/GroupRoutes.js
+++ server/routes/GroupRoutes.js
@@ -1,11 +1,12 @@
 import GroupCtrl from '../controllers/GroupCtr'
+import Auth from '../middleware/Auth'
 
 const GroupRoutes = (router) => {
     router.route('/group')
       .post(GroupCtrl.createGroup);
 
     router.route('/group/:id/user')
-      .post(GroupCtrl.addUserToGroup)
+      .post( GroupCtrl.addUserToGroup)
       .get(GroupCtrl.retrieveGroupUsers);
 
 }
