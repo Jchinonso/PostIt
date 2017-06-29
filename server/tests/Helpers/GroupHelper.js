270b373f59ee2e7323677200bb3e8159a0351aa0
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-06-29 13:06:23 +0100

    feat: dummydatas for test

---
+++ server/tests/Helpers/GroupHelper.js
@@ -0,0 +1,14 @@
+import faker from 'faker';
+
+module.exports = {
+  goodGroup1: {
+    name: 'lagos',
+    description: 'talks about andela Nigeria',
+  },
+
+  goodGroup2: {
+    name: 'nairobi',
+    description: 'talks about andela Kenya'
+  }
+
+};
