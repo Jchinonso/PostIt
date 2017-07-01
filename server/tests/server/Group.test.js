6d88434c27c497219e022b8d41eed009cb90a6fb
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-07-01 19:54:11 +0100

    feature: Added test case for group that already exist

--- server/tests/server/Group.test.js
+++ server/tests/server/Group.test.js
@@ -1,51 +1,52 @@
-import expect from 'expect';
 import chai from 'chai';
 import supertest from 'supertest';
 import app from '../../config/app';
 import db from '../../models';
 import groupHelper from '../Helpers/GroupHelper';
 
-
+const expect = chai.expect;
 const request = supertest(app);
-let groupResponse;
+const goodGroup = groupHelper.goodGroup;
+const badGroup = groupHelper.badGroup;
+let newGroupResponse;
 
-
-describe('Group API', () => {
-  describe('create Group', () => {
-    beforeEach((done) => {
-      request.post('/api/group')
-        .send(groupHelper.goodGroup1)
-        .end((err, res) => {
-          if (err) return err;
-          groupResponse = res;
-          done();
-        });
-    });
-
-    afterEach(() => db.Groups.destroy({ where: {} }));
-
-    it('should create a new group', (done) => {
-      expect(groupResponse.status).toBe(200);
-      expect(groupResponse.body.group).to.have.property(
-        'name');
-      expect(groupResponse.body.group).to.have.property(
-        'description');
-      expect(groupResponse.body.message).to.equal(
-        'Group successfully created');
+describe('POST api/group', () => {
+  beforeEach((done) => {
+    request.post('/api/group')
+    .send(goodGroup)
+    .end((err, res) => {
+      if (err) return err;
+      newGroupResponse = res;
       done();
     });
-
-
-    it('should not create the Group if it already exists', (done) => {
-      request.post('/api/group')
-        .send(groupHelper.goodGroup1)
-        .end((err, response) => {
-          if (err) return err;
-          expect(response.status).to.equal(409);
-          expect(response.body.message).to.equal(
-            'Group already exist');
-          done();
-        });
+  });
+  afterEach(() => db.Groups.destroy({ where: {} }));
+  it('should create a new group', (done) => {
+    expect(newGroupResponse.status).to.equal(200);
+    expect(newGroupResponse.body).to.have.property('name');
+    expect(newGroupResponse.body).to.have.property('description');
+    done();
+  }); 
+  it('should have a createdAt property', (done) => {
+    expect(newGroupResponse.body).to.have.property('createdAt');
+    done();
+  });
+  it('should not create group with missing property ', (done) => {
+    request.post('/api/group')
+    .send(badGroup)
+    .end((err, res) => {
+      expect(res.body.message).to.equal('property mising');
+      return done();
+    });
+  });
+  it('should not create a group if already exist ', (done) => {
+    request.post('/api/group')
+    .send(goodGroup)
+    .end((err, res) => {
+      expect(res.status).to.equal(409);
+      expect(res.body.message).to.equal('Group already exist');
+      return done();
     });
   });
 });
+
