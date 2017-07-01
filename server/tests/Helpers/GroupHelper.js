54522ea332c6f8d2386f76571cef550297b773da
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-07-01 19:44:59 +0100

    feature: Added dummy datas for test cases

--- server/tests/Helpers/GroupHelper.js
+++ server/tests/Helpers/GroupHelper.js
@@ -1,14 +1,13 @@
 import faker from 'faker';
 
 module.exports = {
-  goodGroup1: {
-    name: 'lagos',
-    description: 'talks about andela Nigeria',
+  goodGroup: {
+    name: 'logo',
+    description: 'talks about andela logo',
   },
 
-  goodGroup2: {
-    name: 'nairobi',
-    description: 'talks about andela Kenya'
+  badGroup: {
+    description: 'incomplete'
   }
 
 };
---
+++ server/tests/Helpers/UserHelper.js
@@ -0,0 +1,19 @@
+import faker from 'faker';
+
+module.exports = {
+  goodUser: {
+    username: 'johnny19',
+	  email: 'chinonso@example.com',
+    password: 'abacus3456',
+  },
+  userDoesntExist: {
+    username: 'jdoe',
+	  email: 'jdoe@example.com',
+    password: 'abacus3456',
+  },
+  badUser: {
+    username: 'tayo19',
+	  email: 'tayo12@example.com',
+  },
+
+};
