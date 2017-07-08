974d3d01f3d75c26225dbffab32fb34220469314
jchinonso <johnsonchinonso19@gmail.com>
2017-07-08 20:39:25 +0100

    [feat]: required dotenv

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // This will be our application entry. We'll setup our server here.
+import app from './server/config/app';
 
-import app from './server/config/app'
+require('dotenv').config();
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, (err) => {
