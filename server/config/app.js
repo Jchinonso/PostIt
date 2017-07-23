8bc84c29647bd32424664b671ceae2c27f105866
andela-jchinonso <chinonso.johnson@andela.com>
2017-07-23 22:40:32 +0100

    set webpack middleware

--- server/config/app.js
+++ server/config/app.js
@@ -1,14 +1,25 @@
-
-
-import express from 'express';
+import path from 'path';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import express from 'express';
+import webpack from 'webpack';
+import config from '../../webpack.config.dev';
+
 import Routes from '../routes/index';
 
 require('dotenv').config();
 
+/* eslint-disable no-console */
 const app = express();
 const router = express.Router();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -19,8 +30,8 @@ Routes(router);
 // prefix /api for all routes
 app.use('/api', router);
 
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+app.get('*', (req, res) => res.status(200).sendFile(path.join(
+  __dirname, '../../client', 'index.html')
+));
 
 module.exports = app;
