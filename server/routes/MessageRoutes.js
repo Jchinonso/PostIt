c463029a64c7f874951710733c43c7918cf3eedf
andela-jchinonso <chinonso.johnson@andela.com>
2017-08-04 11:12:55 +0100

    add middleware to secure routes

--- server/routes/MessageRoutes.js
+++ server/routes/MessageRoutes.js
@@ -1,9 +1,10 @@
-import MessageCtrl from '../controllers/MessageCtr'
+import MessageCtrl from '../controllers/MessageCtr';
+import Auth from '../middleware/Auth';
 
 const MessageRoutes = (router) => {
-     router.route('/group/:id/message')
-      .post(MessageCtrl.createMessage)
-      .get(MessageCtrl.retrieveAllMessages);
-}
+  router.route('/group/:id/message')
+    .post(Auth.verifyToken, MessageCtrl.createMessage)
+    .get(Auth.verifyToken, MessageCtrl.retrieveAllMessages);
+};
 
 export default MessageRoutes;
