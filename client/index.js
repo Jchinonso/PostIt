3a8b1e17b6df21a400a97ead29c593f1755b40be
andela-jchinonso <chinonso.johnson@andela.com>
2017-09-11 14:14:07 +0100

    [fix]: add new route

--- client/index.js
+++ client/index.js
@@ -5,8 +5,7 @@ import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import App from './Component/App';
 import routes from './routes';
-import './assets/css/dashboard.css';
-import './assets/css/homepage.css';
+import './assets/css/post.css';
 
 const store = configureStore();
 render(<Provider store={store} >
--- client/routes.js
+++ client/routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './Component/App';
-import HomePage from './Component/HomePage';
+import AuthenticationPage from './Component/AuthComponent/index';
+import Groups from './Component/Dashboard/Groups';
+import Dashboard from './Component/Dashboard/Dashboard';
 
 export default(
   <Route path="/" component={App}>
-    <IndexRoute component={HomePage} />
+    <IndexRoute component={AuthenticationPage} />
+    <Route path="/dashboard" component={Groups} />
   </Route>
 );
