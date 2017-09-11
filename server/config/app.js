f54ab116d5b71edd7fd96469c1e2c6b1a008c060
andela-jchinonso <chinonso.johnson@andela.com>
2017-09-11 09:08:46 +0100

    [feature]: add express validation middleware

--- server/config/app.js
+++ server/config/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import expressValidator from 'express-validator';
 import Routes from '../routes/index';
 
 require('dotenv').config();
@@ -11,6 +12,7 @@ const router = express.Router();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 
 Routes(router);
 
