502106a048b40be8a22f791548c7ab6be9dcb89d
andela-jchinonso <chinonso.johnson@andela.com>
2017-09-11 09:02:19 +0100

    [feature]: add validation middleware to routes

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,15 +1,16 @@
 import UsersCtrl from '../controllers/UserCtr';
 import Auth from '../middleware/Auth';
+import Validation from '../middleware/Validation';
 
 const UserRoutes = (router) => {
   router.route('/user')
     .get(Auth.verifyToken, UsersCtrl.showUsers);
 
   router.route('/user/signup')
-    .post(UsersCtrl.signUp);
+    .post(Validation.validateSignUpInput, UsersCtrl.signUp);
 
   router.route('/user/signin')
-    .post(UsersCtrl.signIn);
+    .post(Validation.validateSignInInput, UsersCtrl.signIn);
   router.route('/user/signOut')
     .post(UsersCtrl.signOut);
 };
