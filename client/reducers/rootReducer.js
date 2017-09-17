e1cd16aa3606e6663a449551cb259626d4ba1dcf
andela-jchinonso <chinonso.johnson@andela.com>
2017-09-17 09:41:30 +0100

    [refactor]: refactor state mutatbility

--- client/reducers/rootReducer.js
+++ client/reducers/rootReducer.js
@@ -3,12 +3,20 @@ import auth from './auth';
 import activeGroup from './activeGroup';
 import groups from './groups';
 import activeGroupmessages from './activeGroupmessages';
+import { SIGNOUT_SUCCESS } from '../constants/ActionTypes';
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   auth,
   activeGroup,
   activeGroupmessages,
   groups
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === SIGNOUT_SUCCESS) {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
 
 export default rootReducer;
