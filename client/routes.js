31a664e4499e11054cb669e9227819825781b3e2
andela-jchinonso <chinonso.johnson@andela.com>
2017-09-18 14:30:32 +0100

    [fix]: add authentication middleware

--- client/routes.js
+++ client/routes.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './Component/App';
 import AuthenticationPage from './Component/AuthComponent/index';
-// import MainComponent from './Component/DashBoard/MainComponent/index';
 import DashBoard from './Component/DashBoard/Index';
+
+const onEnter = (next, replace, cb) => {
+  if (localStorage.getItem('tokenize') !== null && next.location.pathname === '/') {
+    replace('/dashboard');
+  }
+  if (localStorage.getItem('tokenize') === null && (next.location.pathname === ('/dashboard'))) {
+    replace('/');
+  }
+  cb();
+};
 
 export default(
   <Route path="/" component={App}>
     <IndexRoute component={AuthenticationPage} />
-    <Route path="/dashboard" component={DashBoard} />
+    <Route path="/dashboard" component={DashBoard} onEnter={onEnter} />
   </Route>
 );
