42901c4b25206bb85f77810f483a1fbfc8166b32
andela-jchinonso <chinonso.johnson@andela.com>
2017-09-19 13:20:01 +0100

    [refactor]: fix connection to database

--- server/config/app.js
+++ server/config/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-import expressValidator from 'express-validator';
 import Routes from '../routes/index';
 
 require('dotenv').config();
@@ -12,7 +11,6 @@ const router = express.Router();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator());
 
 Routes(router);
 
